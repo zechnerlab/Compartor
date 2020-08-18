381121bbca94cb25f50f616202f88c7e8d16614c
tpietzsch <tobias.pietzsch@gmail.com>
2020-08-18 13:53:06 +0200

    fix transition specifications (nested birth death example)

--- pushthrough.py
+++ pushthrough.py
@@ -8,15 +8,15 @@ transition_b = Transition(Compartment(ContentVar('x')), Compartment(ContentVar('
 transition_d = Transition(Compartment(ContentVar('x')), Compartment(ContentVar('x') + ContentChange(-1)))
 
 k_E = Constant('k_E')
-g_E = n(ContentVar('x'))
+g_E = 1
 pi_E = 1
 
 k_b = Constant('k_b')
-g_b = n(ContentVar('x'))
+g_b = 1
 pi_b = 1
 
 k_d = Constant('k_d')
-g_d = n(ContentVar('x')) * ContentVar('x')[0]    # TODO ContentVar('x') should be enough here, in case D=1.
+g_d = ContentVar('x')[0]    # TODO ContentVar('x') should be enough here, in case D=1.
 pi_d = 1
 
 transitions = [
