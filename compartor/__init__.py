1a29c50def205482a58100058645511b66d66806
tpietzsch <tobias.pietzsch@gmail.com>
2020-10-23 14:39:05 +0200

    Define exported names in __init__.py

---
+++ compartor/__init__.py
@@ -0,0 +1,21 @@
+from .compartments import (Content, ContentChange, Compartment, Transition, TransitionClass, Constant,
+                           OutcomeDistribution, Moment, compute_moment_equations)
+from .display import display_transition_classes, display_propensity_details, display_moment_equations
+from .closure import gamma_closure, gamma_closures, substitute_closures
+from .codegen import GenerateJulia, GeneratePython
+
+__all__ = [
+    # compartor.compartments
+    'Content', 'ContentChange', 'Compartment', 'Transition', 'TransitionClass',
+    'Constant', 'OutcomeDistribution', 'Moment', 'compute_moment_equations',
+
+    # compartor.display
+    'display_transition_classes', 'display_propensity_details',
+    'display_moment_equations',
+
+    # compartor.closure
+    'gamma_closure', 'gamma_closures', 'substitute_closures',
+
+    # compartor.codegen
+    'GenerateJulia', 'GeneratePython',
+]
