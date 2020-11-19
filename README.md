28efd8f2e346dd4777c233dab0b71e59e9aeb5bc
tpietzsch <tobias.pietzsch@gmail.com>
2020-11-19 21:07:44 +0100

    Prepare PyPI package

---
+++ README.md
@@ -0,0 +1,21 @@
+# Compartor
+
+Compartor is an automatic moment equation generator for stochastic compartment populations:
+From a set of provided interaction rules, it derives the associated system of ODEs that describes the population statistics.
+Equations can be exported Python or Julia code.
+
+## Installation
+
+TODO
+
+
+## Getting started
+
+TODO
+
+
+## Publications
+
+Duso, L. and Zechner, C. (2020).
+Stochastic reaction networks in dynamic compartment populations.
+*Proceedings of the National Academy of Sciences,* **117**(37), 22674–22683.
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='example-pkg-compartor-tpietzsch',
+    version='0.0.1',
+    packages=['compartor'],
+    url='http://github.com/zechnerlab/Compartor',
+    license='Simplified BSD License',
+
+    description='Automatic moment equation generation for stochastic compartment populations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author='Tobias Pietzsch',
+    author_email='tobias.pietzsch@gmail.com',
+
+    python_requires='>=3',
+    install_requires=['sympy>=1.6'],
+)
+
+# TODO required: sympy 1.6.2
+# TODO python version required? >= 3.5 ?
