0923792b8e37cd87ca7419e7b09b73c9592e3c50
tpietzsch <tobias.pietzsch@gmail.com>
2020-12-06 15:56:49 +0100

    bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='compartor',
-    version='0.9.0',
+    version='1.1.0',
     packages=['compartor'],
     url='http://github.com/zechnerlab/Compartor',
     license='Simplified BSD License',
