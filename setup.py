c235e0794afad82960cf3c7d90a2a8689ebe6578
Tommaso Bianucci <churli@users.noreply.github.com>
2023-04-13 15:40:01 +0200

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='compartor',
-    version='1.1.1',
+    version='1.2.0',
     packages=['compartor'],
     url='http://github.com/zechnerlab/Compartor',
     license='Simplified BSD License',
